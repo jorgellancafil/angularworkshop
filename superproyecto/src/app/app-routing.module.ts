d1778e280281c18641a183e67e9bad3b67cdd748
jllancafil <jorgellancafil@gmail.com>
2020-02-22 12:10:58 -0300

    avances

--- superproyecto/src/app/app-routing.module.ts
+++ superproyecto/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { MainComponent } from './main/main.component';
 
 
 const routes: Routes = [
-  { path: "main", component: MainComponent }
+  { path: "main", component: MainComponent },
+  { path: "**", component: MainComponent }
 
 ];
 
--- superproyecto/src/app/app.module.ts
+++ superproyecto/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { CardComponent } from './card/card.component';
+import { HistoryComponent } from './history/history.component';
+import { GetinfoComponent } from './getinfo/getinfo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    CardComponent,
+    HistoryComponent,
+    GetinfoComponent
   ],
   imports: [
     BrowserModule,
